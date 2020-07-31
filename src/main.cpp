f2a9d1efac68ebd02a35af82fec1cede7caaf324
standarddeviant <dave.crist@gmail.com>
2020-07-31 12:41:42 -0400

    adding wifi, dns, msgpack and websocket code

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,14 @@
 #include <ArduinoJson.h>
 #include <esp_err.h>
 #include <esp_log.h>
-#include <esp_timer.h>
 #include <driver/i2s.h>
 #include <freertos/ringbuf.h>
 #include <opus.h>
-// #include "WIFI_SECRETS.h"
+#include "WIFI_SECRETS.h"
+// if you don't have WIFI_SECRETS.H, fill in and uncomment the following lines
+// and copy it to WIFI_SECRETS.H in the same folder as main.cpp
+// const char* ssid = "WIFI_SSID_FROM_WIFI_SECRETS.H";
+// const char* password = "WIFI_PASSWORD_FROM_WIFI_SECRETS.H";
 
 #define MIC_PIN_CLK  0
 #define MIC_PIN_DATA 34
@@ -24,7 +27,7 @@
 #define MIC_BUF_LEN (MIC_CHANNELS * MIC_BUF_FRAMES) // units of opus_int16
 #define MIC_BUF_BYTES (MIC_BUF_LEN * MIC_BYTES_PER_SAMP)
 opus_int16 g_micBuf[MIC_CHANNELS * MIC_BUF_FRAMES] = { 0 };
-
+opus_int16 g_micBufMin = (opus_int16)32767, g_micBufMax = (opus_int16)-32768;
 
 #define OP_BUF_BYTES (MIC_CHANNELS * MIC_BUF_FRAMES * MIC_BYTES_PER_SAMP)
 #define OP_RINGBUF_ITEM_SIZE OP_BUF_BYTES
@@ -33,6 +36,43 @@ RingbufHandle_t g_opRingbuf;
 uint32_t g_sendCount = 0, g_receiveCount = 0, g_maxItemSize = 0;
 OpusEncoder *g_opEnc;
 
+
+#define WS_SERVER_PORT 42042
+#define WS_SERVER_HOSTNAME "m5c-mic-opus"
+WebSocketsServer webSocket = WebSocketsServer(WS_SERVER_PORT);
+StaticJsonDocument<4000> g_doc;
+#define MPK_MAX_SZ 4000
+char g_mpkBuf[MPK_MAX_SZ];
+size_t g_mpkSize;
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length);
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t length) {
+    switch(type) {
+        case WStype_DISCONNECTED:
+            {
+                Serial.print("WS: We lost one!\n");
+            }
+            break;
+        case WStype_CONNECTED:
+            {
+                Serial.print("WS: We got one!\n");
+				// webSocket.sendTXT(num, "Connected");
+                // TODO - send opus header on connect?
+                // webSocket.sendBIN(num, ???)
+            }
+            break;
+        case WStype_ERROR:
+        case WStype_TEXT:
+        case WStype_BIN:
+        case WStype_FRAGMENT_TEXT_START:
+        case WStype_FRAGMENT_BIN_START:
+        case WStype_FRAGMENT:
+        case WStype_FRAGMENT_FIN:
+        case WStype_PING:
+        case WStype_PONG:
+            break;
+    }
+}
 
 void ringbufInit() {
     g_opRingbuf = xRingbufferCreateNoSplit(OP_RINGBUF_ITEM_SIZE, 2);
@@ -93,6 +133,11 @@ void micOpusTaskFunc( void * pvParameters ) {
             continue;
         }
 
+        for(int ix = 0; ix < MIC_BUF_LEN; ix++) {
+            if(g_micBuf[ix] < g_micBufMin) g_micBufMin = g_micBuf[ix];
+            if(g_micBuf[ix] > g_micBufMax) g_micBufMax = g_micBuf[ix];
+        }
+
         // encode audio from g_micBuf to g_opBuf
         ret = opus_encode(
             g_opEnc,
@@ -118,10 +163,43 @@ void micOpusTaskFunc( void * pvParameters ) {
 
 
 void wifiTaskFunc( void * pvParameters ) {
-    void *item;
-    size_t itemSize;
+    int ix, ret;     // loop and return vars
+    void *item;      // to receive from ringbuf
+    size_t itemSize; // to receive from ringbuf
+    uint8_t *u8ptr;  // to traverse ringbuf item
 
     // setup wifi code here
+
+    // Basic M5Stick-C Init
+    M5.begin();
+    M5.Axp.ScreenBreath(8); // turn the screen 0=off, 8=dim
+    M5.Lcd.setRotation(3);
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setTextSize(1);
+    M5.Lcd.setCursor(0, 0);
+    M5.Lcd.printf("Connecting to WiFi...");
+
+    // WiFi Init
+    WiFi.begin(ssid, password);
+    while(WiFi.status() != WL_CONNECTED) {
+        delay(100);
+    }
+
+    // WebSocket Init
+    webSocket.begin();
+    webSocket.onEvent(webSocketEvent);
+    // DNS Init
+    if(!MDNS.begin(WS_SERVER_HOSTNAME)) {
+        Serial.println("Error starting mDNS");
+    }
+    MDNS.addService("ws", "tcp", WS_SERVER_PORT);
+
+    IPAddress ip = WiFi.localIP();
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setCursor(0, 0);
+    M5.Lcd.setCursor(0, 20);
+    M5.Lcd.printf("Connect to\n%s:%d",
+        WS_SERVER_HOSTNAME, WS_SERVER_PORT);
 
     while(1) {
         item = xRingbufferReceive(g_opRingbuf, &itemSize, pdMS_TO_TICKS(1000));
@@ -130,9 +208,27 @@ void wifiTaskFunc( void * pvParameters ) {
             continue;
         }
         g_receiveCount++;
+
+        // pack opus bytes in to a msgpack buffer
+        g_doc.clear();
+        u8ptr = (uint8_t *)item;
+        JsonArray _op = g_doc.createNestedArray("opus");
+        for(ix = 0; ix < itemSize; ix++)
+            _op.add(*(u8ptr++));
+        g_mpkSize = serializeMsgPack(g_doc, &(g_mpkBuf[0]), sizeof(g_mpkBuf));
+
+        // after packing opus bytes in to msgpack buffer,
+        // "return" the "item" to the ringbuf
         vRingbufferReturnItem(g_opRingbuf, item);
-    }
-}
+
+        // broadcast msgpack containing opus encoded bytes via websocket
+        ret = webSocket.broadcastBIN((uint8_t *) &(g_mpkBuf[0]), g_mpkSize);
+
+        // TODO - check return variable, ret
+
+    } // end while(1)
+
+} // end void wifiTaskFunc( void * pvParameters )
 
 
 void setup() {
@@ -140,9 +236,12 @@ void setup() {
     BaseType_t xReturned;
     TaskHandle_t xHandle = NULL;
 
+    // set up serial to see debug, warning, error messages, etc.
     Serial.begin(115200); delay(10);
 
-    ringbufInit();     // init step 1
+    // set up the ring-buffer to communicate between micOpusTask and wifiTask
+    // NOTE: this must be done before starting micOpusTask and wifiTask
+    ringbufInit();
 
     Serial.printf("starting wifiTask\n");
     /* Create task to test opus encoder */
@@ -174,10 +273,12 @@ void setup() {
     Serial.printf("started micOpusTask with return code %d\n", xReturned);
 
     while(1) {
-        delay(1000);
-        Serial.printf("MAXSZ = %d, SEND = %d, RECEIVE = %d\n",
-                      g_maxItemSize, g_sendCount, g_receiveCount);
+        delay(2000);
+        Serial.printf("MAXSZ = %d, MINVAL = %d, MAXVAL = %d, SEND = %d, RECEIVE = %d\n",
+                      g_maxItemSize, g_micBufMin, g_micBufMax, g_sendCount, g_receiveCount);
         g_maxItemSize = 0;
+        g_micBufMin = (opus_int16) 32767;
+        g_micBufMax = (opus_int16)-32768;
     }
 }
 
